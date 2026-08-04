e6cd1f2ac2fab686517d3a1f068a074824cd3bf8
Mayank Kumar <mayankkushwah40259@gmail.com>
2026-08-04 15:57:39 +0530

    Homework part. 4 of Ques 24.

---
+++ Chap6_Arrays_and_DSA_Start/Thinking_Level_2_Question/Merge2SortedArrays4th.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+
+public class Merge2SortedArrays4th{
+
+    // Program 33). :- Homework part. 4 of Ques 24.
+
+    // Ques. 24).do arrays ko merge karna hai into single arrays but different method.
+    // piche se karenge hm dono arrays ke components ko 1 and j respectively
+    //  tip :- chahe piche se karo ya aage se baat vahi hogi sort apko hi karna hai bs logic ulte ho jayenge .
+
+    public static void main(String[] args){
+
+    }
+}
